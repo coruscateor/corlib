0697da56c53d0236fd7bc84885e4bab50b72c61e
Paul Saunders <coruscateor@users.noreply.github.com>
2024-04-09 19:47:24 +1200

    Added a MovableText enum for when you want to move Strings or a static String slices around.

--- src/text_enums.rs
+++ src/text_enums.rs
@@ -477,3 +477,103 @@ impl ToString for ScopedStaticText<'_>
     }
 
 }
+
+//-// Most of the above will likely be removed.
+
+//MovableText: Ideal for when you want to be able to move text around that could either be a String ot a static String slice.
+pub enum MovableText
+{
+
+    String(String),
+    Str(&'static str)
+
+}
+
+impl MovableText
+{
+
+    pub fn is_string(&self) -> bool
+    {
+
+        if let MovableText::String(_) = self
+        {
+
+            return true;
+
+        }
+
+        false
+
+    }
+
+    pub fn is_str(&self) -> bool
+    {
+
+        if let MovableText::Str(_) = self
+        {
+
+            return true;
+
+        }
+
+        false
+
+    }
+
+}
+
+impl Into<String> for MovableText
+{
+
+    fn into(self) -> String
+    {
+        
+        match self
+        {
+
+            MovableText::String(val) => val,
+            MovableText::Str(val) => val.to_string()
+
+        }
+        
+    }
+
+}
+
+impl AsStr for MovableText
+{
+
+    fn as_str(&self) -> &str
+    {
+       
+        match self
+        {
+
+            MovableText::String(val) => val.as_str(),
+            MovableText::Str(val) => val
+            
+        }
+
+
+    }
+    
+}
+
+impl ToString for MovableText
+{
+
+    fn to_string(&self) -> String
+    {
+
+        match self
+        {
+
+            MovableText::String(val) => val.clone(),
+            MovableText::Str(val) => val.to_string()
+            
+        }
+        
+    }
+
+}
+
